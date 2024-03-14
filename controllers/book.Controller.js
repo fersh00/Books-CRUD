1dfa150c5fdf0366e7e1a8310f2a350efd21bc74
fc <fc323897@gmail>
2024-03-14 16:56:53 -0700

    Se crea bookController.js

---
+++ controllers/book.Controller.js
@@ -0,0 +1,43 @@
+import Book from "../models/Book";
+import Author from "../models/Author";
+
+const createBook = async (req, res) => {
+	try {
+		let authorsData = req.body.authors;
+		const bookData = req.body.book;
+
+		//Validaciones
+		if (!authorsData || !bookData) {
+			res.status(400).json({
+				msg: 'Authors data or book data is missing',
+			})
+		}
+		if (!Array.isArray(authorsData)) {
+			res.status(400).json({
+				msg: 'Authors data must ba an array',
+			})
+		}
+
+		authorsData = authorsData.map(author => {
+			return new Author(author);
+		})
+
+		const newBook = await Book.create({
+			genre: bookData.genre,
+			isbn: bookData.isbn,
+			title: bookData.title,
+			year: bookData.year,
+			authors: authorsData,
+		});
+
+		res.json(newBook);
+
+	} catch (error) {
+		res.status(500).json({
+			msg: 'Error creating Book',
+			error,
+		})
+	}
+}
+
+export { createBook }
